86a236797fe146b18e1ee7e5db4ffcb1b7830fba
Joyce Lee <joycelee901@gmail.com>
2021-05-20 11:11:25 -0500

    commit tree b7cf0619b827476f5c9fea960a93d6785e4dc1d7

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(function(accumulator, element){ 
+    return element + accumulator}, 0)
+
+/*
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 }
+];
+
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+console.log(couponLocations.reduce(couponCounter, 0));*/
